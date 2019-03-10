b2c1d33817fc6f2837d13615c689f78fe844b63f
Drakeen <info@howardfms.com>
2019-03-10 10:17:21 -0600

    more app logic and styling

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function FriendCard(props) {
   return (
     <div className="card">
-        <img src={props.image} text={props.name} alt={props.name} class="img-container" />
+        <img src={props.image} text={props.name} alt={props.name} className="img-container" onClick={() => props.clickCount(props.id)}/>
       {/* <span onClick={() => props.removeFriend(props.id)} className="remove">
         𝘅
       </span> */}
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+  return (
+  <div className="title">
+    <img className="atlogo" src="/images/Adventure_Time_logo.png" alt="logo" />
+    <h1 className="title">{props.children}</h1>
+  </div>);
+  ;
 }
 
 export default Title;
