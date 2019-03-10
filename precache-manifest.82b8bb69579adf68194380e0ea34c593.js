e7a9f1419e0e517b088e554078b12d7be77b065b
Drakeen <info@howardfms.com>
2019-03-10 11:39:32 -0600

    Updates

---
+++ precache-manifest.82b8bb69579adf68194380e0ea34c593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "245500d1f8cac98a8fe3",
+    "url": "/Memory-Game/static/js/runtime~main.8e400c31.js"
+  },
+  {
+    "revision": "a81a292f5de1b88b100d",
+    "url": "/Memory-Game/static/js/main.c644efc5.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/Memory-Game/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "a81a292f5de1b88b100d",
+    "url": "/Memory-Game/static/css/main.9cacdc77.chunk.css"
+  },
+  {
+    "revision": "8854a4fb1d00a869de7a54b054d8eba8",
+    "url": "/Memory-Game/index.html"
+  }
+];
