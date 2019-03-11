ec2246083ed899334199c8892999e0b29ee97ad7
Drakeen <info@howardfms.com>
2019-03-10 20:28:36 -0600

    logo url

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 function Title(props) {
   return (
   <div className="title">
-    <img className="atlogo" src="/images/Adventure_Time_logo.png" alt="logo" />
+    <img className="atlogo" src="https://raw.githubusercontent.com/Gamlilorien/Memory-Game/master/public/images/Adventure_Time_logo.png" alt="logo" />
     <h1 className="title">{props.children}</h1>
   </div>);
   ;
